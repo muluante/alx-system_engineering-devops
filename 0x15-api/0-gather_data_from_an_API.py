76a03397e7a6c77184ace41439073e145b12f94d
muluante <muluante@outlook.com>
2023-04-24 03:06:23 -0700

    0-gather_data_from_an_API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,31 +1,30 @@
 #!/usr/bin/python3
-"""
-returns information about his/her TODO list progress for a
-given employee ID using a REST API
-"""
+"""For a given employee ID, returns information about
+their TODO list progress"""
+
 import requests
-from sys import argv
+import sys
 
-
-def get_todo():
-    """returns employee's TODO list progress"""
-    r_user = requests.get('https://jsonplaceholder.typicode.com/users/{}'
-                          .format(argv[1]))
-
-    r_todo_true = requests.get('https://jsonplaceholder.typicode.com/todos?'
-                               'userId={}&&completed=true'.format(argv[1]))
-    r_todo_false = requests.get('https://jsonplaceholder.typicode.com/todos?'
-                                'userId={}&&completed=false'.format(argv[1]))
-    try:
-        user_dict = r_user.json()
-        true_todo_dict = r_todo_true.json()
-        false_todo_dict = r_todo_false.json()
-        print("Employee {} is done with tasks({}/{}):".
-              format(user_dict['name'], len(true_todo_dict),
-                     len(false_todo_dict) + len(true_todo_dict)))
-        for task in true_todo_dict:
-            print("\t {}".format(task['title']))
-    except:
-        pass
 if __name__ == "__main__":
-    get_todo()
+
+    userId = sys.argv[1]
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                        .format(userId))
+
+    name = user.json().get('name')
+
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    totalTasks = 0
+    completed = 0
+
+    for task in todos.json():
+        if task.get('userId') == int(userId):
+            totalTasks += 1
+            if task.get('completed'):
+                completed += 1
+
+    print('Employee {} is done with tasks({}/{}):'
+          .format(name, completed, totalTasks))
+
+    print('\n'.join(["\t " + task.get('title') for task in todos.json()
+          if task.get('userId') == int(userId) and task.get('completed')]))
