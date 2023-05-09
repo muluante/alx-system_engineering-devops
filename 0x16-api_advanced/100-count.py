ddbb43f9ad89f331d813d54d41f6f158d295990d
muluante <muluante@outlook.com>
2023-05-09 03:24:11 -0700

    100-count

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,55 +1,57 @@
 #!/usr/bin/python3
 """
-parses the title of all hot articles, and prints a sorted count of given
-keywords (case-insensitive, delimited by spaces)
+    Queries Reddit's API to print the number of occurences specified words in
+    all the hot posts for a given subreddit.
 """
-from collections import OrderedDict
-from requests import get
+import requests
 
 
-def count_words(subreddit, word_list, after=None, match_dict={}):
-    """parses the title of all hot articles, and prints a sorted count of given
-    keywords (case-insensitive, delimited by spaces)
+def count_words(subreddit, word_list, after=None, word_counter={}):
+    """ Recursively find all the hot posts for a given Reddit subreddit and
+        tally the number of occurences of words specified in word_list which
+        occur in the titles.
     Args:
-        subreddit (str): subreddit
-        word_list (list): list of words to count occurrences for
-        after (str): refer to next page
-        match_dict (dict): dictionary of frequency of words from word_list
-        flag (int): indicate when match_dict is complete
+        subreddit (str): The name of the subreddit that will be used to query
+        the API.
+        word_list (list of str): List containing words to look for in the
+        titles of hot posts in a subreddit.
+        after (str): ID of the last hot post in the returned result. Used to
+        get the next 100 hot posts after this ID.
     """
-    try:
-        r = get('https://www.reddit.com/r/{}/hot.json?limit=100&&'
-                'after={}'.format(subreddit, after),
-                headers={'User-Agent': 'bc'})
-        sub_dict = r.json()
+    user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36\
+    (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36'
+    headers = {'User-Agent': user_agent}
+    url = 'https://www.reddit.com/r/{}/hot/.json?limit=100'.format(subreddit)
+    if after is not None:
+        url += '&after=' + after
+    resp = requests.get(url,
+                        headers=headers,
+                        allow_redirects=False)
+    if resp.status_code == 200:
+        resp_dict = resp.json()
+        data_dict = resp_dict.get('data', {})
+        a_list = data_dict.get('children', [])
+        for post in a_list:
+            post_dict = post.get('data', {})
+            title = post_dict.get('title')
+            if title is not None:
+                for word in title.lower().split():
+                    for i in range(len(word_list)):
+                        if word_list[i].lower() == word:
+                            word_counter[word_list[i]] = \
+                                word_counter.get(word_list[i], 0) + 1
 
-        if match_dict == {}:
-            for w in word_list:
-                match_dict[w] = 0
-
-        after = sub_dict['data']['after']
-
-        for i in range(len(sub_dict['data']['children'])):
-            title_string = sub_dict['data']['children'][i]['data']['title']
-            search_list = title_string.split()
-            for word in search_list:
-                for w in word_list:
-                    if w.lower() == word.lower():
-                        match_dict[w] += 1
-
+        after = data_dict.get('after')
         if after is None:
-            descend_dict = OrderedDict(sorted(match_dict.items(),
-                                              key=lambda x: x[1],
-                                              reverse=True))
-            zero_count = 0
-            for k, v in descend_dict.items():
-                if v != 0:
-                    print("{}: {}".format(k, v))
-                else:
-                    zero_count += 1
-            if zero_count == len(descend_dict):
-                print()
+            if word_counter != {}:
+                val_dict = {}
+                for key in word_counter:
+                    if val_dict.get(word_counter[key]) is None:
+                        val_dict[word_counter[key]] = [key]
+                    else:
+                        val_dict[word_counter[key]].append(key)
+                for key in sorted(val_dict.keys(), reverse=True):
+                    for word in sorted(val_dict[key]):
+                        print('{}: {}'.format(word, key))
         else:
-            count_words(subreddit, word_list, after, match_dict)
-    except:
-        pass
+            count_words(subreddit, word_list, after, word_counter)
