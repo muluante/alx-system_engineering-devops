35ca58f4eb3bbef7ebd11cce228b9ecf5673f339
muluante <muluante@outlook.com>
2023-05-09 23:34:20 -0700

    1-top_ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
-""" Queries the Reddit API and prints the
-titles of the first 10 hot posts listed for a given subreddit. """
 
-import requests
-headers = {"User-Agent": "ubuntu:hbtn:v1.0 (by /u/piroli_)"}
+import requests as r
 
 
 def top_ten(subreddit):
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    request = requests.get(url, headers=headers, allow_redirects=False)
-
-    if request.status_code == 200:
-        for children in request.json().get("data").get("children"):
-            print(children.get("data").get("title"))
-
-    else:
-        print(None)
+    """Print top 10 post given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:73.0) \
+        Gecko/20100101 Firefox/73.0"
+    }
+    param = {
+        "limit": 10
+    }
+    response = r.get(url, headers=headers, params=param, allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return
+    results = response.json().get("data")
+    [print(top.get("data").get("title")) for top in results.get("children")]
