1732785f95f397f5de295510f75089e1acb5c0c7
muluante <muluante@outlook.com>
2023-08-08 10:26:06 +0000

    new commit

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,73 +1,55 @@
 #!/usr/bin/python3
 """
-    Task 3
+parses the title of all hot articles, and prints a sorted count of given
+keywords (case-insensitive, delimited by spaces)
 """
-import requests
+from collections import OrderedDict
+from requests import get
 
 
-def recurse(subreddit, hot_list=[], after=None):
-    """ gets the number of subscribers """
-    if after is not None:
-        url = 'https://api.reddit.com/r/' +\
-              '{}/hot.json?after={}'.format(subreddit, after)
-    else:
-        url = 'https://api.reddit.com/r/{}/hot.json'.format(subreddit)
-    header = {'user-agent': 'ianscustomthing'}
-
-    r = requests.get(url,
-                     headers=header,
-                     allow_redirects=False)
-    if r.status_code != 200:
-        return None
+def count_words(subreddit, word_list, after=None, match_dict={}):
+    """parses the title of all hot articles, and prints a sorted count of given
+    keywords (case-insensitive, delimited by spaces)
+    Args:
+        subreddit (str): subreddit
+        word_list (list): list of words to count occurrences for
+        after (str): refer to next page
+        match_dict (dict): dictionary of frequency of words from word_list
+        flag (int): indicate when match_dict is complete
+    """
     try:
-        rj = r.json()
-    except:
-        return None
-    if rj.get('message') == 'Not Found':
-        return
-    chldrn = rj.get('data').get('children')
-    hot_list += [chld.get('data').get('title') for chld in chldrn]
-    after = rj.get('data').get('after')
-    if after is not None:
-        return recurse(subreddit, hot_list, after)
-    else:
-        return hot_list
+        r = get('https://www.reddit.com/r/{}/hot.json?limit=100&&'
+                'after={}'.format(subreddit, after),
+                headers={'User-Agent': 'bc'})
+        sub_dict = r.json()
 
+        if match_dict == {}:
+            for w in word_list:
+                match_dict[w] = 0
 
-def print_words(word_list, hot_list):
-    """ prints the words all pretty like """
-    cnts = {}
-    for word in word_list:
-        c = 0
-        for title in hot_list:
-            t = title.lower().split()
-            if word.lower().strip() in t:
-                c += t.count(word.lower().strip())
-            '''
-            for w in title.split():
-                if word.strip().lower() in word.strip().lower():
-                    print("{}, {}".format(word.
-                                          strip().lower(), w.strip().lower()))
-                    c = c + 1
-            '''
-        if word.lower() in cnts:
-            cnts[word.lower()] += c
+        after = sub_dict['data']['after']
+
+        for i in range(len(sub_dict['data']['children'])):
+            title_string = sub_dict['data']['children'][i]['data']['title']
+            search_list = title_string.split()
+            for word in search_list:
+                for w in word_list:
+                    if w.lower() == word.lower():
+                        match_dict[w] += 1
+
+        if after is None:
+            descend_dict = OrderedDict(sorted(match_dict.items(),
+                                              key=lambda x: x[1],
+                                              reverse=True))
+            zero_count = 0
+            for k, v in descend_dict.items():
+                if v != 0:
+                    print("{}: {}".format(k, v))
+                else:
+                    zero_count += 1
+            if zero_count == len(descend_dict):
+                print()
         else:
-            cnts.update({word.lower(): c})
-    '''
-    print(hot_list)
-    cnts.update({'thing': 17})'''
-    for k, v in sorted(cnts.items(), key=lambda x: (-x[1], x[0])):
-        if cnts.get(k) != 0:
-            print("{}: {}".format(k, v))
-
-
-def count_words(subreddit, word_list):
-    """ counts dem keywords """
-    hot_list = recurse(subreddit)
-    if hot_list is None:
-        return
-    f = 0
-    if f == 1:
-        count_words(subreddit, word_list)
-    print_words(word_list, hot_list)
+            count_words(subreddit, word_list, after, match_dict)
+    except:
+        pass
